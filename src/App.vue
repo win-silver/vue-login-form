5b46df032053968a428c462c4e298acf659e3c04
연승은 <seungeun_yeon@tmax.co.kr>
2021-10-18 00:31:07 +0900

    add: data 바인딩 및 submit 이벤트 처리 추가

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
-  <form action="">
+  <form v-on:submit="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username"/>
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="password" />
+      <input id="password" type="password" v-model="password"/>
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
 
 export default {
+  data: function() {
+    return {
+      username: "",
+      password: ""
+    }
+  },
+  methods: {
+    submitForm: function(event) {
+      event.preventDefault();
+      console.log(this.username, this.password);
+    }
+  }
   
 }
 </script>
